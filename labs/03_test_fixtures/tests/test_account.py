0670f6735073637403327f575e8b5d0a95ffc153
Its-suLav-D <dah19001@byui.edu>
2022-07-29 12:02:44 -0600

    Recent Changes

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -12,20 +12,42 @@ class TestAccountModel(TestCase):
     """Test Account Model"""
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls):     # runs once before test case 
         """ Connect and load data needed by tests """
+        global ACCOUNT_DATA 
+
+        with open('tests/fixtures/account_data.json') as json_data: 
+            ACCOUNT_DATA = json.load(json_data)
+
+        db.create_all() 
 
     @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(cls):  # runcs once after test case 
         """Disconnect from database"""
+        db.session.close()
 
-    def setUp(self):
+    def setUp(self): # runs before each test 
         """Truncate the tables"""
+        db.session.query(Account).delete()
+        db.session.commit()
 
-    def tearDown(self):
+    def tearDown(self): # runs after each test 
         """Remove the session"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
+    def test_create_account(self):
+        data = ACCOUNT_DATA[0]
+        account = Account(**data)
+        account.create()
+        self.assertIsNotNone(account.id)
+        self.assertEqual(len(Account.all()), 1)
+
+    def test_create_all_accounts(self):
+        for account in ACCOUNT_DATA:
+            account = Account(**account)
+            account.create()
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))
